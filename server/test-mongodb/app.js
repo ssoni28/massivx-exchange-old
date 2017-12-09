47099063caa88e6eefb27dd8ba5afb986650fa00
soni28 <soni28@ccs.neu.edu>
2017-12-08 21:34:02 -0500

    Heroku setup

--- server/test-mongodb/app.js
+++ server/test-mongodb/app.js
@@ -16,7 +16,7 @@ module.exports = function(app)
     var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
     var password = process.env.MLAB_PASSWORD_WEBDEV;
     connectionString = 'mongodb://' + username + ':' + password;
-    connectionString += '@ds133094.mlab.com:33094/heroku_nqbhvct4'; // use yours
+    connectionString += '@ds133876.mlab.com:33876/heroku_jgn6jx3d'; // use yours
   }
 
   var mongoose = require("mongoose");
